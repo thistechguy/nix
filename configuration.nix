727929bfc65fc109893e7c9b3b07394758d3c49c
thistechguy <86990020+thistechguy@users.noreply.github.com>
2023-04-25 12:22:01 -0400

    Update configuration.nix
    Fixed duplicate printing values.

--- configuration.nix
+++ configuration.nix
@@ -72,9 +72,6 @@ in {
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
